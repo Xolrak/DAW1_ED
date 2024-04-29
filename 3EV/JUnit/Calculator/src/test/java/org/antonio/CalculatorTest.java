b5fa3c4016a8e344cb251433948d9450b8c2a706
Carlos Castañeda <carloscodvcf@gmail.com>
2024-04-29 20:19:08 +0200

    Actualizacion de repositorio

--- 3EV/JUnit/Calculator/src/test/java/org/antonio/CalculatorTest.java
+++ 3EV/JUnit/Calculator/src/test/java/org/antonio/CalculatorTest.java
@@ -3,4 +3,11 @@ package org.antonio;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class CalculatorTest {
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testDivideException() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(3, 2));
+        calc.divide(5, 0);
+    }
 }
