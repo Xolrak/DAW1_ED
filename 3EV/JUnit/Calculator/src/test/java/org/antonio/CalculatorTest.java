0e9deb250c4d4d6bf1abe7c98f45629b949552d2
Carlos Castañeda <carloscodvcf@gmail.com>
2024-05-22 00:01:35 +0200

    calculadora v2

--- 3EV/JUnit/Calculator/src/test/java/org/antonio/CalculatorTest.java
+++ 3EV/JUnit/Calculator/src/test/java/org/antonio/CalculatorTest.java
@@ -2,12 +2,54 @@ package org.antonio;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class CalculatorTest {
 
-    @Test (expected = IllegalArgumentException.class)
-    public void testDivideException() {
-        Calculator calc = new Calculator();
-        assertEquals(5, calc.add(3, 2));
-        calc.divide(5, 0);
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(2, -3));
+        //
+        assertNotEquals(4, calculator.add(2, 3));
+        assertNotEquals(0, calculator.add(2, -3));
+
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.subtract(2, 3));
+        assertEquals(5, calculator.subtract(2, -3));
+        //
+        assertNotEquals(-2, calculator.subtract(2, 3));
+        assertNotEquals(4, calculator.subtract(2, -3));
+
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(12, calculator.multiply(2, 6), 0.001);
+        assertEquals(-6, calculator.multiply(2, -3), 0.001);
+        //
+        assertNotEquals(12, calculator.multiply(2, 6), 0.001); 
+        assertNotEquals(6, calculator.multiply(2, -3), 0.001);   
+
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        assertEquals(2.0f, calculator.divide(6, 3), 0.001);
+        assertEquals(-2.0f, calculator.divide(6, -3), 0.001);
+        //
+        assertNotEquals(3.0f, calculator.divide(6, 3), 0.001);
+        assertNotEquals(2.0f, calculator.divide(6, -3), 0.001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        calculator.divide(6, 0);
     }
 }
